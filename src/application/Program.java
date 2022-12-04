ea3fdd033c91a8f09f53ee02e6d03330d183a4e8
Onun216 <nunomdf216@gmail.com>
2022-12-04 11:41:12 +0000

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,11 +11,13 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
+		//Department obj = new Department(1, "Books");
 		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		//Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
-		SellerDao sellerDao = DaoFactory.createSellerDao(); // injecção de dependêcia sem explicitar a implementação
+		SellerDao sellerDao = DaoFactory.createSellerDao(); // injecção de dependência sem explicitar a implementação
+		
+		Seller seller = sellerDao.findById(3);
 				
 				
 		//System.out.println(obj);
