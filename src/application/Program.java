28129022391a0b87551c48db195684fd3e37f13c
Onun216 <nunomdf216@gmail.com>
2022-12-04 12:10:33 +0000

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,18 +13,22 @@ public class Program {
 	public static void main(String[] args) {
 		
 		//Department obj = new Department(1, "Books");
+		//System.out.println(obj);
 		
 		//Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao(); // injecção de dependência sem explicitar a implementação
 		
-		System.out.println("=== TEST 1: seller findById ====");
+		System.out.println("\n=== TEST 1: seller findById ====");
 		Seller seller = sellerDao.findById(3);
-				
-				
-		//System.out.println(obj);
 		System.out.println(seller);
 		
+		System.out.println("\n=== TEST 2: seller findByDepartment ====");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
